740f7fccfea7bc1d1fd4d5e4bc05b859b47e07ec
ing-arriola <ingeniero.arriola@gmail.com>
2020-02-23 06:02:02 -0600

    added fetch for JSON

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+//button to txt
 document.getElementById("txtBtn").addEventListener("click",cargarData)
+//Button to process json
+document.getElementById("jsonBtn").addEventListener("click",cargarJson)
+//This is to show an answer
 const res=document.getElementById("resultado")
+
+//Functions
 function cargarData(){
     fetch("datos.txt")
         .then((ans)=>{
@@ -10,5 +16,20 @@ function cargarData(){
         })
         .catch((error)=>{
             res.innerHTML=error
+        })
+}
+//Load JSON
+function cargarJson(){
+    fetch('empleados.json')
+        .then((connection)=>{
+            return connection.json()
+        })
+        .then((data)=>{
+            data.forEach(element => {
+                console.log(element)
+                let employee=document.createElement("li")
+                employee.innerHTML=`Nombre: ${element.nombre}, puesto: ${element.puesto}`
+                res.appendChild(employee)
+            });
         })
 }
